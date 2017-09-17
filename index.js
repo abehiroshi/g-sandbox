25d5d4cfbf547b5dcc4bacda780e0b94b60581e9
Abe Hiroshi <abhrsh@gmail.com>
2017-09-17 17:40:03 +0000

    Change data into body, headers, query

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const PubSub = require(`@google-cloud/pubsub`);
 
 exports.acceptHttp = function(req, res) {
-  let data
-  if (typeof(req.body) === 'string') {
-    data = Buffer.from(req.body)
-  } else if (Buffer.isBuffer(req.body)) {
-    data = req.body
-  } else {
-    data = Buffer.from(JSON.stringify(req.body))
-  }
-  
   PubSub().topic('receive-http')
     .publisher()
-    .publish(data, {
-      protocol: req.protocol,
-      method: req.method,
-      url: req.originalUrl,
-      headers: JSON.stringify(req.headers),
-      query: JSON.stringify(req.query)
-    })
+    .publish(
+      Buffer.from(JSON.stringify({
+        body: req.body,
+        headers: req.headers,
+        query: req.query
+      })),
+      {
+        protocol: req.protocol,
+        method: req.method,
+        contentType: req.get('content-type')
+      })
     .catch(err => console.error(err))
   
   return res.status(200).end();
 }
 
 exports.acceptPubsub = function(event, callback) {
-  if (Buffer.isBuffer(event.data.data)) {
-    console.log(`data: '<Buffer>' attributes: ${JSON.stringify(event.data.attributes)}`)
-  } else {
-    console.log(`data: ${Buffer.from(event.data.data, 'base64').toString()} attributes: ${JSON.stringify(event.data.attributes)}`)
-  }
+  console.log(`data: ${Buffer.from(event.data.data, 'base64').toString()} attributes: ${JSON.stringify(event.data.attributes)}`)
   callback()
 }
