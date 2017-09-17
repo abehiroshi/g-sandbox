823af5f61877f8a891b163b0b2b3b40d0d5c0ecc
Abe Hiroshi <abhrsh@gmail.com>
2017-09-17 06:25:39 +0000

    change status 200

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ exports.accept = function(req, res) {
   // Example input: {"message": "Hello!"}
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required.
-    res.status(400).send('No message defined!');
+    console.log(req.body);
+    res.status(200).send('No message defined!');
   } else {
     // Everything is okay.
     console.log(req.body.message);
